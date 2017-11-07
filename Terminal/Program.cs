eda041d57aeb825a17a8d5e465dd34270e4623f9
Paulo Brandão <32689830+paulobrandaooficial@users.noreply.github.com>
2017-11-07 21:31:05 -0200

    Commit 4

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -10,9 +10,12 @@ namespace Terminal
         {   
 
             ContaPessoaFisica contapf = new ContaPessoaFisica();
-            
+
             Console.WriteLine(contapf.Sacar(5880.00));
-                       
+
+            ContaPessoaJuridica contapj = new ContaPessoaJuridica();
+                        
+            Console.WriteLine(contapj.Sacar(5880.00));                       
 
         }
     }
